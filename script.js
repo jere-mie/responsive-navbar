978a2570a54b88a52c3cf26d6855f406506336d4
Jeremie <jeremiejbornais@gmail.com>
2020-08-25 17:42:54 -0400

    right section now responsive

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+const navbarLinksRight = document.getElementsByClassName("navbar-links-right")[0];
 
 toggleButton.addEventListener("click", () => {
     navbarLinks.classList.toggle("active");
+    navbarLinksRight.classList.toggle("active");
 })
